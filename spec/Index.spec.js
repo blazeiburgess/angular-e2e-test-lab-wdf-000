989b05d1cbd50ead9665511a14e0944661387d54
blaze.i.burgess@gmail.com <blaze.i.burgess@gmail.com>
2016-08-24 16:27:20 -0400

    Done.

--- spec/Index.spec.js
+++ spec/Index.spec.js
@@ -1,7 +1,25 @@
 var path = require('path');
 
 describe('angularjs homepage todo list', function() {
-	it('should add a new contact', function() {
-		browser.get('http://localhost:8080');
-	});
+  it('should add a new contact', function() {
+    browser.get('http://localhost:8080');
+    // element(by.model('contact.name')).sendKeys('Am Pm')
+    // element(by.model('contact.phone')).sendKeys('1234567890')
+    // element(by.css('.button')).click();
+
+    element(by.model('contact.name')).sendKeys('Tim')
+    element(by.model('contact.phone')).sendKeys('3934203242')
+    element(by.css('.button')).click();
+
+    // element(by.model('contact.name')).sendKeys('Am Pm')
+    // element(by.model('contact.phone')).sendKeys('1234567890')
+    // element(by.css('.button')).click();
+    //
+    // element(by.model('contact.name')).sendKeys('Am Pm')
+    // element(by.model('contact.phone')).sendKeys('1234567890')
+    // element(by.css('.button')).click();
+    //
+    var contactList = element.all(by.repeater('contact in vm.contacts'));
+    expect(contactList.count()).toEqual(4);
+  });
 });
